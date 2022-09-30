6b351a6646393042a14d0a2fef586f75dbd5b8d2
nyhwbh <87648321+nyhwbh@users.noreply.github.com>
2022-10-01 03:14:02 +0900

    change struct name and add skill array

--- OOPver/app/component/charactor.go
+++ OOPver/app/component/charactor.go
@@ -8,20 +8,21 @@ import (
 
 //캐맄터 스테이터스
 type CharacterStatus struct {
-	level              int
-	healthPoint        int
-	maxHealthPoint     int
-	magicPoint         int
-	maxMagicPoint      int
-	basicAttackPoint   int
-	battleAttackPoint  int
-	basicAttackSpeed   int
-	battleAttackSpeed  int
-	basicDefencePoint  int
-	battleDefencePoint int
-	basicAvoid         int
-	battleAvoid        int
-	species            int
+	level                int
+	healthPoint          int
+	maxHealthPoint       int
+	magicPoint           int
+	maxMagicPoint        int
+	basicAttackPower     int
+	battleAttackPower    int
+	basicAttackSpeed     int
+	battleAttackSpeed    int
+	basicDefensivePower  int
+	battleDefensivePower int
+	basicAvoid           int
+	battleAvoid          int
+	species              int
+	characterSkills      []string
 }
 
 // 케릭터 생성
@@ -33,10 +34,10 @@ func (c *CharacterStatus) BasicSetup() {
 	c.healthPoint = c.maxHealthPoint
 	c.maxMagicPoint = random.Intn(50) + 50
 	c.magicPoint = c.maxMagicPoint
-	c.basicAttackPoint = random.Intn(10) + 20
-	c.battleAttackPoint = c.basicAttackPoint
-	c.basicDefencePoint = random.Intn(10) + 10
-	c.battleDefencePoint = c.basicDefencePoint
+	c.basicAttackPower = random.Intn(10) + 20
+	c.battleAttackPower = c.basicAttackPower
+	c.basicDefensivePower = random.Intn(10) + 10
+	c.battleDefensivePower = c.basicDefensivePower
 	c.basicAvoid = 30
 	c.battleAvoid = c.basicAvoid
 	c.basicAttackSpeed = 1000
@@ -54,11 +55,11 @@ func (c *CharacterStatus) SetWeapon(selection int) {
 	case 1:
 		switch selection {
 		case 1:
-			c.battleAttackPoint = c.basicAttackPoint
+			c.battleAttackPower = c.basicAttackPower
 		case 2:
-			c.battleAttackPoint = int(float64(c.basicAttackPoint) * 1.05)
+			c.battleAttackPower = int(float64(c.basicAttackPower) * 1.05)
 		case 3:
-			c.battleAttackPoint = int(float64(c.basicAttackPoint) * 1.1)
+			c.battleAttackPower = int(float64(c.basicAttackPower) * 1.1)
 		}
 	case 2:
 		switch selection {
@@ -72,19 +73,33 @@ func (c *CharacterStatus) SetWeapon(selection int) {
 	case 3:
 		switch selection {
 		case 1:
-			c.battleAttackPoint = c.basicAttackPoint
+			c.battleAttackPower = c.basicAttackPower
 			c.battleAttackSpeed = c.basicAttackSpeed
 		case 2:
-			c.battleAttackPoint = int(float64(c.basicAttackPoint) * 1.10)
+			c.battleAttackPower = int(float64(c.basicAttackPower) * 1.10)
 			c.battleAttackSpeed = int(float64(c.basicAttackSpeed) * 0.95)
 		case 3:
-			c.battleAttackPoint = int(float64(c.basicAttackPoint) * 1.2)
+			c.battleAttackPower = int(float64(c.basicAttackPower) * 1.2)
 			c.battleAttackSpeed = int(float64(c.basicAttackSpeed) * 0.9)
 		}
 	}
 }
 
+// 스킬 생성
+func (c *CharacterStatus) CreateSkills() {
+	characterSkills := []string{"Heal", "steam"}
+	switch c.species {
+	case 1:
+		characterSkills = append(characterSkills, "Guard", "invincible")
+	case 2:
+		characterSkills = append(characterSkills, "Elusion", "Rapid")
+	case 3:
+		characterSkills = append(characterSkills, "Anger", "Frenzy")
+	}
+	c.characterSkills = characterSkills
+}
+
 //케릭터 상태 출력
 func (c *CharacterStatus) PrintCharterStatus() {
-	fmt.Printf("Lv: %d HP: %d / %d MP: %d / %d ATK: %d DEF: %d ATKS: %d \n", c.level, c.healthPoint, c.maxHealthPoint, c.magicPoint, c.maxMagicPoint, c.battleAttackPoint, c.battleDefencePoint, c.battleAttackSpeed)
+	fmt.Printf("Lv: %d HP: %d / %d MP: %d / %d ATK: %d DEF: %d\n", c.level, c.healthPoint, c.maxHealthPoint, c.magicPoint, c.maxMagicPoint, c.battleAttackPower, c.battleDefensivePower)
 }
