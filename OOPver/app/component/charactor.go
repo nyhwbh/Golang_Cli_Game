89908b7f43934208b6b90e080d59acbc319aaa52
nyhwbh <87648321+nyhwbh@users.noreply.github.com>
2022-10-02 03:25:08 +0900

    add skill functions

--- OOPver/app/component/charactor.go
+++ OOPver/app/component/charactor.go
@@ -25,6 +25,7 @@ type CharacterStatus struct {
 	characterSkills      []string
 }
 
+// ----------- 기본 설정 함수 ----------- //
 // 케릭터 생성
 func (c *CharacterStatus) BasicSetup() {
 	timeSource := rand.NewSource(time.Now().UnixNano())
@@ -87,19 +88,124 @@ func (c *CharacterStatus) SetWeapon(selection int) {
 
 // 스킬 생성
 func (c *CharacterStatus) CreateSkills() {
-	characterSkills := []string{"Heal", "steam"}
+	characterSkills := []string{"1.Heal", "2.Steam"}
 	switch c.species {
 	case 1:
-		characterSkills = append(characterSkills, "Guard", "invincible")
+		characterSkills = append(characterSkills, "3.Guard", "4.Invincible")
 	case 2:
-		characterSkills = append(characterSkills, "Elusion", "Rapid")
+		characterSkills = append(characterSkills, "3.Elusion", "4.Rapid")
 	case 3:
-		characterSkills = append(characterSkills, "Anger", "Frenzy")
+		characterSkills = append(characterSkills, "3.Anger", "4.Frenzy")
 	}
 	c.characterSkills = characterSkills
 }
 
+// ----------- 전투관련 함수 ----------- //
+// 케릭터가 공격 받음
+func (c *CharacterStatus) CharacterAttacked(damage int) {
+	c.healthPoint -= damage
+}
+
+// ----------- 스킬 사용 함수 ----------- //
+// 스킬 사용
+func (c *CharacterStatus) ApplySkill(selection int) {
+	mpCheck := c.UsingMagicPoint()
+	if mpCheck {
+		switch selection {
+		case 1:
+			c.SkillHeal()
+		case 2:
+			c.SkillSteam()
+		case 3:
+			switch c.species {
+			case 1:
+				c.SkillGuard()
+			case 2:
+				c.SkillElusion()
+			case 3:
+				c.SkillAnger()
+			}
+		case 4:
+			switch c.species {
+			case 1:
+				c.SkillInvincible()
+			case 2:
+				c.SkillRapid()
+			case 3:
+				c.SkillFrenzy()
+			}
+		}
+	} else {
+		fmt.Println("MP가 부족하여 스킬을 사용할 수 없습니다.")
+	}
+}
+
+// MP 차감
+func (c *CharacterStatus) UsingMagicPoint() bool {
+	timeSource := rand.NewSource(time.Now().UnixNano())
+	random := rand.New(timeSource)
+	usedMP := random.Intn(10) + 10
+	check_mp := c.magicPoint - usedMP
+	if check_mp <= 0 {
+		return false
+	} else {
+		c.magicPoint = check_mp
+		return true
+	}
+}
+
+// Skills
+func (c *CharacterStatus) SkillHeal() {
+	if (c.healthPoint + 30) >= c.maxHealthPoint {
+		c.healthPoint = c.maxHealthPoint
+	} else {
+		c.healthPoint += 30
+	}
+}
+
+func (c *CharacterStatus) SkillSteam() {
+	c.battleAttackPower = int(float64(c.battleAttackPower) * 1.2)
+}
+
+func (c *CharacterStatus) SkillGuard() {
+	c.battleDefensivePower = int(float64(c.basicDefensivePower) * 1.3)
+}
+
+func (c *CharacterStatus) SkillInvincible() {
+	//10초 동안 무적이 됨
+}
+
+func (c *CharacterStatus) SkillElusion() {
+	c.battleAvoid = int(float64(c.basicAvoid) * 1.3)
+}
+
+func (c *CharacterStatus) SkillRapid() {
+	// 1분동안
+	c.battleAttackSpeed = int(float64(c.battleAttackSpeed) / 5)
+}
+
+func (c *CharacterStatus) SkillAnger() {
+	c.battleAttackPower = int(float64(c.battleAttackPower) * 1.5)
+	c.battleDefensivePower = int(float64(c.basicDefensivePower) * 0.9)
+}
+
+func (c *CharacterStatus) SkillFrenzy() {
+	// 1분동안
+	c.battleAttackPower = int(float64(c.battleAttackPower) * 5)
+
+}
+
+// ----------- 출력 함수 ----------- //
 //케릭터 상태 출력
 func (c *CharacterStatus) PrintCharterStatus() {
 	fmt.Printf("Lv: %d HP: %d / %d MP: %d / %d ATK: %d DEF: %d\n", c.level, c.healthPoint, c.maxHealthPoint, c.magicPoint, c.maxMagicPoint, c.battleAttackPower, c.battleDefensivePower)
+}
+
+// 스킬 출력
+func (c *CharacterStatus) PrintSkills() {
+	if c.level < 99 {
+		fmt.Println(c.characterSkills[0:3])
+	} else {
+		fmt.Println(c.characterSkills)
+	}
 }
